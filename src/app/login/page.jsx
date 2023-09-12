02aa44e7a1e4020a2f4614b9370a4e0181f9c720
Godfred Akpan <godfredakpan@gmail.com>
2023-09-12 13:50:31 +0100

    update route

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
-import {getUser, loginUser} from '../../services/index'
+import {getUser, loginUser, setUserSession} from '../../services/index'
 
 const Login = () => {
   const [email, setEmail] = useState('');
